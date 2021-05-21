3c956ad46765443e6a8aeb88f5ce4403aa562160
duanjial <carlo.duan@gmail.com>
2021-05-21 14:33:31 -0400

    modified directory

--- downloader.py
+++ downloader.py
@@ -8,24 +8,24 @@ import argparse
 
 
 def download_m3u8_file(url, file_name):
-    if not os.path.exists("./downloadM3U8/tmp"):
-        os.makedirs("./downloadM3U8/tmp")
-    file = os.path.join("./downloadM3U8/tmp", file_name)
+    if not os.path.exists("./tmp"):
+        os.makedirs("./tmp")
+    file = os.path.join("./tmp", file_name)
     with urllib.request.urlopen(url) as response, open(file, "wb") as outFile:
         shutil.copyfileobj(response, outFile)
 
 
 def download_mp4(file_name):
-    if not os.path.exists("./downloadM3U8/videos"):
-        os.makedirs("./downloadM3U8/videos")
+    if not os.path.exists("./videos"):
+        os.makedirs("./videos")
     os.system(
         f"ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto \
-             -i ./downloadM3U8/tmp/{file_name} -c copy ./downloadM3U8/videos/{file_name[:-5]}.mp4"
+             -i ./tmp/{file_name} -c copy ./videos/{file_name[:-5]}.mp4"
     )
 
 
 def add_home_url_to_m3u8_file(home_url, file_name):
-    with open(os.path.join("./downloadM3U8/tmp", file_name), "r") as file:
+    with open(os.path.join("./tmp", file_name), "r") as file:
         lines = file.readlines()
 
     def add_prefix(line):
@@ -35,7 +35,7 @@ def add_home_url_to_m3u8_file(home_url, file_name):
             return line
 
     data = list(map(add_prefix, lines))
-    with open(os.path.join("./downloadM3U8/tmp", file_name), "w") as file:
+    with open(os.path.join("./tmp", file_name), "w") as file:
         file.writelines(data)
 
 
